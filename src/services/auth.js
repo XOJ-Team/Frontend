e826da5f36ca14c2e007ee2dfe56dd8f956625c6
Yusen Wu <sanmusen_wu@outlook.com>
2022-03-13 16:14:41 +0800

    feat login and register service function

--- src/services/auth.js
+++ src/services/auth.js
@@ -1,6 +1,18 @@
 // 用户登录登出服务类
 import {post} from '../utils/request'
 
-// export function loginApi(user){
-//     return post("/api/login",user)
-// }
+/**
+ * user login
+ * return the response of server,you can add .then() to use callback function
+ */
+export function loginApi(user){
+    return get("/api/login",user)
+}
+
+/**
+ * user register
+ * return the response of server,you can add .then() to use callback function
+ */
+ export function registerApi(user){
+    return post("/api/register",user)
+}
