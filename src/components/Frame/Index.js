fa5fd98cf75a8f7bc9de2319434468f8a180d86e
Yusen Wu <sanmusen_wu@outlook.com>
2022-03-14 00:46:40 +0800

    Update Tabbar style

--- src/components/Frame/Index.js
+++ src/components/Frame/Index.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Layout, Menu, Breadcrumb } from 'antd';
+import { Layout, Menu, Breadcrumb, Button } from 'antd';
 import "./Index.css";
 import { useNavigate } from 'react-router-dom';
 // 路由寻找
@@ -7,54 +7,64 @@ import { findRoute } from '../../routers/config';
 
 const { Header, Content, Footer } = Layout;
 
-function Index(props){
-    let navigate=useNavigate()
-    const menuItems=[{
-        name:"Main",
-        targeturl:"/"
-    },{
-        name:"Questions",
-        targeturl:"/questions"
-    },{
-        name:"Login",
-        targeturl:"/login"
+function Index(props) {
+    let navigate = useNavigate()
+    const menuItems = [{
+        name: "Main",
+        targeturl: findRoute('mainpage')
+    }, {
+        name: "Questions",
+        targeturl: findRoute('questionList')
     }]
     // console.log(props.history.location.pathname)
-        return (
-            <Layout className="layout">
-                <Header>
-                    <div className="logo" style={{float:"left",padding:"0px 20px"}}>
-                        <img src='favicon.ico' />
-                    </div>
-                    <Menu 
-                    theme="dark" 
-                    mode="horizontal" 
+    return (
+        <Layout className="layout">
+            <Header>
+                <div className="logo" style={{ position:'absolute', padding: "0px 20px" }}>
+                    <img src='favicon.ico' />
+                </div>
+                <Menu
+                    theme="dark"
+                    mode="horizontal"
                     defaultSelectedKeys={['1']}
                     selectedKeys={[]}
-                    >
-                        {()=>{console.log(props.history)}}
-                        {menuItems.map((item, index) => {
-                            return <Menu.Item key={item.targeturl} onClick={(e)=>{
+                    style={{ paddingLeft: '130px' }}
+                >
+                    {() => { console.log(props.history) }}
+                    {menuItems.map((item, index) => {
+                        return <Menu.Item
+                            key={item.targeturl}
+                            onClick={(e) => {
                                 navigate(e.key)
                             }}>{item.name}</Menu.Item>;
-                        })}
-                    </Menu>
-                </Header>
-                <Content style={{ padding: '50px 0px' }}>
-                    {/* <Breadcrumb style={{ margin: '16px 0' }}>
+                    })}
+                    <Menu.Item
+                        style={{ position: 'absolute', right: '0px', padding:'0 30px'}}
+                        onClick={(e)=>{
+                            navigate(findRoute('userlogin'))
+                        }}
+                    >
+                        <a>Login</a>
+                    </Menu.Item>
+                </Menu>
+
+
+            </Header>
+            <Content style={{ padding: '50px 0px' }}>
+                {/* <Breadcrumb style={{ margin: '16px 0' }}>
                         <Breadcrumb.Item>Home</Breadcrumb.Item>
                         <Breadcrumb.Item>List</Breadcrumb.Item>
                         <Breadcrumb.Item>App</Breadcrumb.Item>
                     </Breadcrumb> */}
-                    <div className="site-layout-content">
-                        {props.children}
-                    </div>
+                <div className="site-layout-content">
+                    {props.children}
+                </div>
 
-                </Content>
-                <Footer style={{ textAlign: 'center' }}>Ant Design ©2018 Created by Ant UED</Footer>
-            </Layout>
-        )
-    }
+            </Content>
+            <Footer style={{ textAlign: 'center' }}>Ant Design ©2018 Created by Ant UED</Footer>
+        </Layout>
+    )
+}
 
 
 export default Index;
