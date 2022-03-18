71ca97a76e4cf0453aad44dd9f5444526f4d63f2
Yusen Wu <sanmusen_wu@outlook.com>
2022-03-18 15:33:40 +0800

    Feat: edit hardness and title

--- src/pages/questions/EditQ.js
+++ src/pages/questions/EditQ.js
@@ -1,9 +1,10 @@
 // 题目创建以及编辑页面
+// 只有拥有管理员权限才能看到这页
 import React, { useState } from 'react';
 import * as ReactDOM from 'react-dom';
-// import style manually
+// UI import style manually
 import 'react-markdown-editor-lite/lib/index.css';
-
+import { Form, Input, Button, Select } from 'antd';
 // utils
 import { useLocation } from 'react-router-dom';
 import MarkdownIt from 'markdown-it';
@@ -16,21 +17,53 @@ import qs from 'qs'
 // Initialize a markdown parser
 const mdParser = new MarkdownIt(/* Markdown-it options */);
 
-// Finish!
+// Form
+const { Option } = Select;
+
+const layout = {
+  labelCol: { span: 8 },
+  wrapperCol: { span: 8 },
+};
+const tailLayout = {
+  wrapperCol: { offset: 8, span: 16 },
+};
 
 export default function EditQ(props) {
+  // Form
+  const [form] = Form.useForm();
+
+  const onGenderChange = (value: string) => {
+    switch (value) {
+      case 'male':
+        form.setFieldsValue({ note: 'Hi, man!' });
+        return;
+      case 'female':
+        form.setFieldsValue({ note: 'Hi, lady!' });
+        return;
+      case 'other':
+        form.setFieldsValue({ note: 'Hi there!' });
+    }
+  };
+
+  const onFinish = (values: any) => {
+    console.log(values);
+  };
+
+  const onReset = () => {
+    form.resetFields();
+  };
+
   // 获取url传来的题目id
   let remindword = ''
   let location = useLocation()
   let params = qs.parse(location.search.slice(1))
-  let iscreate = true
-  if ('id' in params) {
-    // 编辑已有题目
-    remindword = 'You are eding question:' + params.id
-
-  } else {
+  let iscreate = 'id' in params?false:true
+  if (iscreate) {
     // 创建新题目
-    remindword = 'You are creating question'
+    remindword = 'You are creating new question'
+  } else {
+    // 编辑已有题目
+    remindword = 'You are editng question:' + params.id
   }
 
   // 编辑内容的双向绑定
@@ -43,10 +76,48 @@ export default function EditQ(props) {
 
 
   return (
+    <div>
     <MdEditor
       value={mdword}
       style={{ height: '500px' }}
       renderHTML={text => mdParser.render(text)}
       onChange={handleEditorChange} />
+      <Form {...layout} form={form} name="control-hooks" onFinish={onFinish}>
+      <Form.Item name="title" label="Title" rules={[{ required: true }]}>
+        <Input />
+      </Form.Item>
+      <Form.Item name="hard" label="Hard" rules={[{ required: true }]}>
+        <Select
+          placeholder="Select how hard it is"
+          onChange={onGenderChange}
+          allowClear
+        >
+          <Option value="male">easy</Option>
+          <Option value="female">medium</Option>
+          <Option value="other">hard</Option>
+        </Select>
+      </Form.Item>
+      <Form.Item
+        noStyle
+        shouldUpdate={(prevValues, currentValues) => prevValues.gender !== currentValues.gender}
+      >
+        {({ getFieldValue }) =>
+          getFieldValue('gender') === 'other' ? (
+            <Form.Item name="customizeGender" label="Customize Gender" rules={[{ required: true }]}>
+              <Input />
+            </Form.Item>
+          ) : null
+        }
+      </Form.Item>
+      <Form.Item {...tailLayout}>
+        <Button type="primary" htmlType="submit">
+          Submit
+        </Button>
+        <Button htmlType="button" onClick={onReset}>
+          Reset
+        </Button>
+      </Form.Item>
+    </Form>
+    </div>
   );
 };
