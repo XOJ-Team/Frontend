7eef3fc13cc3d1e46a4857756f253f6860524a18
Yusen Wu <sanmusen_wu@outlook.com>
2022-03-21 18:52:53 +0800

    Feat: context store user id

--- src/contexts/AuthContext.js
+++ src/contexts/AuthContext.js
@@ -5,7 +5,7 @@ import React, {createContext,useState} from 'react'
 // 跨组件传参父组件
 
 /**
- * 传递pUsername,pAuthority
+ * 传递pUsername,pAuthority,pUserid
  * 
  * 使用方法：
  * let obj=useContext(Auth)
@@ -16,14 +16,20 @@ export const Auth = createContext()
 // 包装时接受children
 export function AuthContext({children}){
     // 要传递的信息
+    // 用户名
     const [pUsername,setpUsername]=useState(null)
-    const [pAuthority,setpAuthority]=useState(null)
+    // 用户id
+    const [pUserid,setpUserid]=useState(null)
+    // 用户权限 1普通用户2能创建比赛3管理员
+    const [pAuthority,setpAuthority]=useState(3)
+
     return (
     // 传进来的children作为子组件渲染
     // value里放传递的信息和回调
     <Auth.Provider value={{
         pUsername,setpUsername,
-        pAuthority,setpAuthority
+        pAuthority,setpAuthority,
+        pUserid,setpUserid
         }}>
         {children}
     </Auth.Provider>
