c8ac21b744a7a17e46305441d61a88c1933113dc
Yusen Wu <sanmusen_wu@outlook.com>
2022-04-04 21:04:57 +0800

    Feat 404 page

--- src/pages/NotFound.js
+++ src/pages/NotFound.js
@@ -1,8 +1,26 @@
 import React from 'react'
+//UI
+
+//utils
+import { useNavigate } from 'react-router-dom';
+import { findRoute } from '../routers/config';
 
 export default function NotFound() {
+  const navigate=useNavigate()
   return (
-    <div>NotFound</div>
+    <div>
+      <div style={{textAlign:'center'}}
+      ><img src='/404.jpg' width={'250em'}/></div>
+      <div
+      style={{
+      fontSize:"2em",
+      textAlign:"center"
+      }}>
+        Nothing is here
+        <br />
+        <a onClick={()=>{navigate(findRoute("mainpage"))}}>Back to home</a>
+        </div>
+    </div>
   )
 }
 
