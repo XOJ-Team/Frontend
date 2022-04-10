bfd224bd72b2b24693ad3f957d7dae6fcc77a0d4
chenzhengde <felix.zdchen@gmail.com>
2022-04-10 20:52:04 +0800

    Update interfaces (user and manager)

---
+++ src/services/manager.js
@@ -0,0 +1,49 @@
+import {get, post, put, del} from '../utils/request'
+
+/**
+ * delete account
+ * @param obj.mail(str)
+ */
+ export function delManagerAccount(obj){
+    return post("/manager/delete", obj)
+}
+
+/**
+ * insert user
+ * @param obj.id(Long)
+ * @param obj.verificationNumber(str)
+ * @param obj.name(str)
+ * @param obj.mail(str)
+ * @param obj.phoneNumber(str)
+ * @param obj.password(str)
+ * @param obj.score(str)
+ * @param obj.ranking(str)
+ * @param obj.authority(str)
+ */
+export function insertManagerUser(obj){
+    return post("/manager/insert", obj)
+}
+
+/**
+ * manager modify user information
+ * @param obj.id(Long)
+ * @param obj.verificationNumber(str)
+ * @param obj.name(str)
+ * @param obj.mail(str)
+ * @param obj.phoneNumber(str)
+ * @param obj.password(str)
+ * @param obj.score(str)
+ * @param obj.ranking(str)
+ * @param obj.authority(str)
+ */
+ export function modifyManagerUser(obj){
+    return post("/manager/modify", obj)
+}
+
+/**
+ * get user infomation
+ *
+ */
+export function getUserInfoManager(obj){
+    return get("/manager/user", obj)
+}
---
+++ src/services/userInfo.js
@@ -0,0 +1,33 @@
+import {get, post, put, del} from '../utils/request'
+
+/**
+ * delete account
+ * @param obj.mail(str)
+ */
+export function delAccount(obj){
+    return post("/user/delete", obj)
+}
+
+/**
+ * get user' information by id
+ * @param obj.id(str)
+ */
+export function getUserInfo(obj){
+    return post("/user/info", obj)
+}
+
+/**
+ * user modify itself
+ * @param obj.id(Long)
+ * @param obj.verificationNumber(str)
+ * @param obj.name(str)
+ * @param obj.mail(str)
+ * @param obj.phoneNumber(str)
+ * @param obj.password(str)
+ * @param obj.score(str)
+ * @param obj.ranking(str)
+ * @param obj.authority(str)
+ */
+export function modifyUserInfo(obj){
+    return post("/user/modify", obj)
+}
