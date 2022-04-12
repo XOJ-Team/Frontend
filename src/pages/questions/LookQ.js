8b6b29da4b95ee17d81065a3f497ad5f074bf7d2
Cenxn <1017435995@qq.com>
2022-04-12 21:38:20 +0800

    master

--- src/pages/questions/LookQ.js
+++ src/pages/questions/LookQ.js
@@ -7,9 +7,10 @@ import { useLocation,useNavigate } from 'react-router-dom';
 import qs from 'qs'
 import {findRoute} from '../../routers/config'
 import {selectQuestionId} from '../../services/question'
-import { message,PageHeader,Button,Tag } from 'antd';
+import { message,PageHeader,Button,Tag, Row, Col, Divider, List, Typography, Card } from 'antd';
 import { getTestcase } from '../../services/testcase';
 import DocumentTitle from 'react-document-title'//动态Title
+import './LookQ.css';
 
 const mdParser = new MarkdownIt(/* Markdown-it options */);
 
@@ -20,9 +21,11 @@ export default function LookQ() {
   const [questionHard,setquestionHard] = useState(null)
   const [tags,settags]=useState("")
   const [testcases,settestcases]=useState([])
+  const [creatorName,setcreatorName]=useState("")
 
   // 困难标签的颜色
-  const whichcolor={'easy':'green','medium':'orange','hard':'red'}
+  // const whichcolor={'easy':'green','medium':'orange','hard':'red'}
+  const whichcolor={'easy':'success','medium':'warning','hard':'danger'}
 
 
   // 获取url传来的题目id
@@ -39,6 +42,7 @@ export default function LookQ() {
           setquestionHard(e.data.obj.levelDescription)
           settags(e.data.obj.tags)
           setmdword(e.data.obj.content)
+          setcreatorName(e.data.obj.creatorName)
         }else{
           message.error('error id:'+params['id'])
         }
@@ -55,30 +59,27 @@ export default function LookQ() {
       message.warn("should have a id in url")
     }
   },[])
-  
 
   return (
   <DocumentTitle title="XOJ | Question">
-    <div>
+    <Row 
+    gutter={26}
+    >
+      <Col span={16} offset={1}>
     <PageHeader
-    title={questionTitle}
+    title={params['id']+". "+questionTitle}
     onBack={()=>{navigate(-1)}}
-    />
-    <div style={{padding:"0px 50px"}}>
-    <Tag color={whichcolor[questionHard]}>{questionHard}</Tag>
-    <div>
-      <span style={{color:'gray'}}>Tags:</span>
-      {tags.split("#").map((item,index)=>{
-      return <Tag key={index}>{item}</Tag>
-    })}
-    </div>
-
+    style={{
+      padding:"10px 0px 30px 30px",
+    }}
+    >
+    <div style={{padding:"0px 10px"}}>
     {/* <ReactMarkdown children={mdword} /> */}
+    <Divider orientation="left">Question Description</Divider>
     <div 
     style={{
-      border:'1px solid black',
       borderRadius:'10px',
-      padding:'0px 30px',
+      padding:'0px 20px 0px 10px',
       overflow:'auto',
       // 超出长度自动换行
       tableLayout:'fixed',
@@ -86,21 +87,63 @@ export default function LookQ() {
       wordWrap:'break-word'
     }}
     dangerouslySetInnerHTML={{__html:mdParser.render(mdword)}}></div>
-
-    <div>testcase:</div>
-    {testcases.map((e)=>{return <div>input: {e.testcase},result: {e.result}</div>})}
-
-    <Button 
-    type='primary'
-    onClick={()=>{
-      // navigate()
-    }}
+    <div style={{
+      textAlign:'center',
+      padding:'10px',
+      }}>
+      <Button 
+      type='primary'
+      onClick={()=>{
+        // navigate()
+      }}
+      >
+        Start to write
+      </Button>
+    </div>
+    <Divider orientation="left">Test Case Example</Divider>
+    {testcases.map((e)=>{return <div style={{
+      padding:'0px 30px',
+      }}>
+        <Card.Grid 
+        style={{
+          padding:'10px 20px', 
+          width: '30%',
+          height:'200px',
+          overflow:'auto',
+          tableLayout:'fixed',
+          wordBreak:'break-all',
+          wordWrap:'break-word',
+          backgroundColor:'rgba(1,5,68,0.1)',
+          borderRadius:'15px;' }}>
+      <p><b>Input:</b><br/>{e.testcase}<br/><b>Result:</b><br/>{e.result}</p>
+      </Card.Grid></div>})}
+    </div>
+    <Divider />
+    </PageHeader>
+    </Col>
+    <Col span={5}>
+    {/* <div>
+      <div>Difficulty: <Tag color={whichcolor[questionHard]}>{questionHard}</Tag></div>
+      <span style={{color:'gray'}}>Tags:</span>
+      {tags.split("#").map((item,index)=>{
+      return <Tag key={index}>{item}</Tag>
+    })}
+    </div> */}
+    <List
+    header={<b>Question Information</b>}
+    itemLayout="horizontal"
     >
-      Start to write
-    </Button>
-    </div>
-
-    </div>
+      <List.Item>Question ID: {params['id']}</List.Item>
+      <List.Item>Created By: <a>{creatorName}</a></List.Item>
+      <List.Item><div>Difficulty: <Typography.Text type={whichcolor[questionHard]}>{questionHard.toUpperCase()}</Typography.Text></div></List.Item>
+      <List.Item><div>Tags: <br/>
+      {tags.split("#").map((item,index)=>{
+      return <Tag style={{margin:"5px 5px 10px 5px"}} key={index}>{item}</Tag>
+    })}</div>
+      </List.Item>
+    </List>
+    </Col>
+    </Row>
     </DocumentTitle>
   )
 }
