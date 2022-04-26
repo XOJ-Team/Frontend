0813bad9ef29815d163d1e5809c3380a20f3bc41
Yusen Wu <sanmusen_wu@outlook.com>
2022-04-26 10:33:29 +0800

    perf: lazy load and preload

--- src/routers/config.js
+++ src/routers/config.js
@@ -1,21 +1,57 @@
+// React.lazy() and preload
+// https://github.com/pomber/react-lazy-preload-demo/pull/8/commits/126c1bf6e2a23b3e3f7eb0ffb1b4db260516643f
 // v6路由
+import React from 'react';
 import {Navigate} from "react-router-dom";
-// 记录所有路由配置
-import ListQ from '../pages/questions/ListQ';
-import MainPage from '../pages/openpage/MainPage';
-import LookQ from '../pages/questions/LookQ';
-import Login from '../pages/LoginRegister/Login';
-import NotFound from '../pages/NotFound';
-import Register from '../pages/LoginRegister/Register';
-import EditQ from '../pages/questions/EditQ';
-import UserPage from '../pages/userpages/UserPage';
-import ModifyInfo from "../pages/userpages/ModifyInfo";
-import Aboutus from '../pages/About/Aboutus';
-import ViewCompetition from '../pages/Competition/ViewCompetition'
-import ListCompetition from '../pages/Competition/ListCompetition'
-import SiteRank from '../pages/Ranks/SiteRank';
-import EditCompetition from '../pages/Competition/EditCompetition';
-import Managepage from "../pages/Management/Managepage";
+
+
+
+// 预懒加载所有路由
+const ListQPromise = import(/* webpackChunkName: 'ListQ' */ '../pages/questions/ListQ')
+const ListQ=React.lazy(()=>ListQPromise)
+
+const MainPagePromise = import(/* webpackChunkName: 'MainPage' */ '../pages/openpage/MainPage')
+const MainPage=React.lazy(()=>MainPagePromise)
+
+const LookQPromise = import(/* webpackChunkName: 'LookQ' */ '../pages/questions/LookQ')
+const LookQ=React.lazy(()=>LookQPromise)
+
+const LoginPromise = import(/* webpackChunkName: 'Login' */ '../pages/LoginRegister/Login')
+const Login=React.lazy(()=>LoginPromise)
+
+const RegisterPromise = import(/* webpackChunkName: 'Register' */ '../pages/LoginRegister/Register')
+const Register=React.lazy(()=>RegisterPromise)
+
+const EditQPromise = import(/* webpackChunkName: 'EditQ' */ '../pages/questions/EditQ')
+const EditQ=React.lazy(()=>EditQPromise)
+
+const UserPagePromise = import(/* webpackChunkName: 'UserPage' */ '../pages/userpages/UserPage')
+const UserPage=React.lazy(()=>UserPagePromise)
+
+const ModifyInfoPromise = import(/* webpackChunkName: 'ModifyInfo' */ '../pages/userpages/ModifyInfo')
+const ModifyInfo=React.lazy(()=>ModifyInfoPromise)
+
+const AboutusPromise = import(/* webpackChunkName: 'Aboutus' */ '../pages/About/Aboutus')
+const Aboutus=React.lazy(()=>AboutusPromise)
+
+const NotFoundPromise = import(/* webpackChunkName: 'NotFound' */ '../pages/NotFound')
+const NotFound=React.lazy(()=>NotFoundPromise)
+
+const ViewCompetitionPromise = import(/* webpackChunkName: 'ViewCompetition' */ '../pages/Competition/ViewCompetition')
+const ViewCompetition=React.lazy(()=>ViewCompetitionPromise)
+
+const ListCompetitionPromise = import(/* webpackChunkName: 'ListCompetition' */ '../pages/Competition/ListCompetition')
+const ListCompetition=React.lazy(()=>ListCompetitionPromise)
+
+const EditCompetitionPromise = import(/* webpackChunkName: 'EditCompetition' */ '../pages/Competition/EditCompetition')
+const EditCompetition=React.lazy(()=>EditCompetitionPromise)
+
+const SiteRankPromise = import(/* webpackChunkName: 'SiteRank' */ '../pages/Ranks/SiteRank')
+const SiteRank=React.lazy(()=>SiteRankPromise)
+
+const ManagepagePromise = import(/* webpackChunkName: 'Managepage' */ '../pages/Management/Managepage')
+const Managepage=React.lazy(()=>ManagepagePromise)
+
 
 // 路由配置
 // id应当唯一,最好不要更改id
