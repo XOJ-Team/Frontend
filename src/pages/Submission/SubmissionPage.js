6c3038a2db374f81fd30e4a1d1a10896d27c0a02
Yusen Wu <sanmusen_wu@outlook.com>
2022-05-14 13:59:05 +0800

    Feat: submission Page

---
+++ src/pages/Submission/SubmissionPage.js
@@ -0,0 +1,72 @@
+import React, { useEffect,useState } from 'react'
+import { useLocation,useNavigate } from 'react-router-dom'
+// UI
+import DocumentTitle from 'react-document-title'
+// utils
+import { findRoute } from '../../routers/config'
+import { Auth } from '../../contexts/AuthContext'
+import qs from 'qs'
+import { Base64toUtf8 } from '../../utils/base64'
+import { oneRecord } from '../../services/submitRecord'
+
+export default function SubmissionPage() {
+
+    const [info,setinfo]=useState({})
+    // 获取url传来的题目id
+    let location = useLocation()
+    let params = qs.parse(location.search.slice(1))
+    const paramsid=params['id']
+    let navigate=useNavigate()
+
+    useEffect(()=>{
+        getSubInfo(paramsid)
+    },[])
+
+    // 获取subid提交记录的信息，记录在info变量里
+    const getSubInfo=(subid)=>{
+        oneRecord({"recordId":subid}).then((res)=>{
+            if(res.data.status==1){
+                setinfo(res.data.obj)
+            }
+        })
+    }
+
+    return (
+        <DocumentTitle title="XOJ | Submissions">
+            <div class="componentbox">
+            <div>
+                codes:
+                {Base64toUtf8(info.codes)}
+            </div>
+            <div>
+                createTime:
+                {
+                    info.createTime?
+                    info.createTime.substring(0,19).replace("T"," "):null
+                }
+            </div>
+            <div>
+                question name:
+                <a onClick={()=>{navigate(findRoute('questionOnlyOne')+"?id="+info.questionId)}}>{info.questionName}</a>
+            </div>
+            <div>
+                resultDescription:
+                {info.resultDescription}
+            </div>
+            <div>
+                time cost:
+                {info.timeCost+"MS"}
+            </div>
+            <div>
+                memory cost:
+                {info.memoryCost+"KB"}
+            </div>
+            <div>
+                language:
+                {info.lang}
+            </div>
+        </div>
+        </DocumentTitle>
+
+    )
+}
--- src/utils/base64.js
+++ src/utils/base64.js
@@ -2,6 +2,17 @@ import crypto from 'crypto-js'
 
 
 export function Utf8toBase64(text){
+    if(!text){
+        return ""
+    }
     const wordArray=crypto.enc.Utf8.parse(text)
     return crypto.enc.Base64.stringify(wordArray)
+}
+
+export function Base64toUtf8(text){
+    if(!text){
+        return ""
+    }
+    const wordArray=crypto.enc.Base64.parse(text)
+    return crypto.enc.Utf8.stringify(wordArray)
 }
