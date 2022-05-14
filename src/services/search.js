a0f635f3501f3e40fdef1e5f54f8615061bed052
Yusen Wu <sanmusen_wu@outlook.com>
2022-05-14 17:25:24 +0800

    Fix: es search

--- src/services/search.js
+++ src/services/search.js
@@ -1,46 +1,6 @@
 import {get, post,put,del} from '../utils/request'
 
 /**
- * delete a document by name
- * @param field(str)
- * @param id(str)
- * @param index(str)
- * @param obj
- */
-export function delDocumentName(obj){
-    return post("/es/delete", obj)
-}
-
-/**
- * insert a document
- * @param field(str)
- * @param id(str)
- * @param index(str)
- * @param obj
- */
- export function insertDocument(obj){
-    return post("/es/insert", obj)
-}
-
-/**
- * show es head
- */
- export function insertDocument(){
-    return get("/es/show")
-}
-
-/**
- * notice! this update is implemented by insert (will cover origin data
- * @param field(str)
- * @param id(str)
- * @param index(str)
- * @param obj
- */
- export function insertDocument(obj){
-    return post("/es/update", obj)
-}
-
-/**
  * synchronize the mysql and elasticsearch
  */
 export function refreshES(){
